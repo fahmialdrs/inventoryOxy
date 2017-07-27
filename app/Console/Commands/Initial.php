d53e04fe210a90eba3e6b08b1a75fde949b22f68
fahmialdrs <fahmialdrs@yahoo.com>
2017-07-27 18:32:48 +0700

    command initial admin

---
+++ app/Console/Commands/Initial.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Role;
+use App\User;
+
+class Initial extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Inisialisasi User Admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //create admin
+        $adminRole = new Role;
+        $adminRole->name = 'admin';
+        $adminRole->display_name = 'Admin';
+        $adminRole->save();
+
+        // create admin
+        $admin = new User;
+        $admin->name = 'Admin';
+        $admin->email = 'a@a.com';
+        $admin->password = bcrypt('123456');
+        $admin->save();
+        $admin->attachRole($adminRole);
+
+        if ($admin) {
+             $this->info('Sukses inisialisasi admin');
+        }
+        else{
+            $this->error('Gagal inisialisasi admin');
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\CheckReminder'
+        'App\Console\Commands\CheckReminder',
+        'App\Console\Commands\Initial'
     ];
 
     /**
