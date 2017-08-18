e251311e751b2850706aa4a8aa54b11baff04066
Fahmi Alaydrus <fahmialdrs@gmail.com>
2017-08-18 14:05:11 +0700

    fixing tab service

--- resources/views/ujiriksa/index.blade.php
+++ resources/views/ujiriksa/index.blade.php
@@ -37,6 +37,88 @@
 								</a>
 							</li>							
 						</ul>
+						<div class="tab-pane" role="tabpanel" id="table_service">
+							<table id="service" class="display">
+							    <thead>
+							        <tr>
+							        	<th>No Registrasi Uji</th>
+							            <th>Jenis Uji</th>
+							            <th>Progress</th>
+							            <th>Keterangan</th>
+							            <th>Tanggal Masuk</th>
+							            <th>Tanggal Selesai</th>
+							            <th>Nama Pemilik</th>
+							            <th>Aksi</th>
+							        </tr>
+							    </thead>
+							    <tbody>
+							    @foreach($service as $fu)
+							        <tr>
+							        	<td><a href="{{ route('ujiriksa.show', $fu->id) }}">{{ $fu->no_registrasi }}</a></td>
+							            <td>{{ $fu->jenis_uji }}</td>
+							        	<td>{{ $fu->progress }}</td>
+							        	<td>{{ $fu->keterangan }}</td>
+							            <td>{{ $fu->created_at->format('d-M-Y') }}</td>
+							            @if(isset($fu->done_at))
+							            <td>{{ $fu->done_at->format('d-M-Y') }}</td>
+							            @else
+							            <td>{{ "Belum Diinput" }}</td>
+							            @endif
+							            <td><a href="{{ route('customer.show', $fu->customer->id) }}">{{ $fu->customer->nama }}</a></td>
+							            <td>
+							            	<div class="btn-group dropdown" role="group" aria-label="...">
+											  <div class="btn-group navbar-right">
+												  <button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												    Aksi <span class="caret"></span>
+												  </button>
+												  <ul class="dropdown-menu ">
+												  <li class="dropdown-header">Aksi</li>
+												  @if($fu->progress == 'Selesai')@if(!isset($fu->itemujiriksa->first()->serviceresult))
+												  	<li>
+														<a type="button" href="{{ route('service.create', $fu->id) }}">Input Hasil Service</a>
+												  	</li>
+												  @endif
+												  @endif
+												  	<!-- <li>
+														<a type="button" href="{{ route('ujiriksa.edit', $fu->id) }}">Edit</a>
+												  	</li> -->
+												  	<li>
+														<a type="button" href="{{ route('ujiriksa.destroy', $fu->id)}}" onclick="return confirm('Apakah Anda Ingin Menghapus Data?')">Delete</a>
+												  	</li>											  	
+												  	@if($fu->progress == 'Waiting List')
+												  	<li role="separator" class="divider"></li>
+												  	<li class="dropdown-header">Ubah Status Progress</li>
+												    <li><a href="{{ route('ujiriksa.changeStatus', $fu->id) }}">Dikerjakan</a></li>
+												    @elseif($fu->progress == 'Sedang Dikerjakan')
+												    <li role="separator" class="divider"></li>
+												  	<li class="dropdown-header">Ubah Status Progress</li>
+												    <li><a href="{{ route('ujiriksa.changeStatus', $fu->id) }}">Selesai</a></li>
+												    @elseif($fu->progress == 'Selesai')
+												    @if(!isset($fu->nama_pengambil))
+												    <li role="separator" class="divider"></li>
+												  	<li class="dropdown-header">Input Nama Pengambil</li>
+												    <li><a href="#" onclick="myFunction()">Diambil</a></li>
+												    <form id="form" role="form" action="{{ route('ujiriksa.storePengambil', $fu->id)}}" method="post">
+													{!! csrf_field() !!}
+												    	<input type="hidden" id="pengambil" name="nama_pengambil" />
+													</form>
+													@endif
+												    @else
+												    @endif
+												    <li role="separator" class="divider"></li>
+												    <li class="dropdown-header">Aksi Tambahan</li>
+												    <li><a href="{{ route('ujiriksa.exportPdf', $fu->id) }}" target="_blank">Export PDF</a></li>
+												    <!-- <li><a href="#">Kirim Email</a></li> -->
+												    
+												  </ul>
+												</div>
+											</div>
+							            </td>
+							        </tr>
+							    @endforeach
+							    </tbody>
+							</table>
+						</div>
 						<div class="tab-content">
 							<div class="tab-pane active" role="tabpanel" id="table_hydro">
 								<table id="hydro" class="display">
@@ -162,88 +244,6 @@
 													  @if(!isset($fu->itemujiriksa->first()->visualresult))
 													  	<li>
 															<a type="button" href="{{ route('visualstatic.create', $fu->id) }}">Input Hasil Visual</a>
-													  	</li>
-													  @endif
-													  @endif
-													  	<!-- <li>
-															<a type="button" href="{{ route('ujiriksa.edit', $fu->id) }}">Edit</a>
-													  	</li> -->
-													  	<li>
-															<a type="button" href="{{ route('ujiriksa.destroy', $fu->id)}}" onclick="return confirm('Apakah Anda Ingin Menghapus Data?')">Delete</a>
-													  	</li>											  	
-													  	@if($fu->progress == 'Waiting List')
-													  	<li role="separator" class="divider"></li>
-													  	<li class="dropdown-header">Ubah Status Progress</li>
-													    <li><a href="{{ route('ujiriksa.changeStatus', $fu->id) }}">Dikerjakan</a></li>
-													    @elseif($fu->progress == 'Sedang Dikerjakan')
-													    <li role="separator" class="divider"></li>
-													  	<li class="dropdown-header">Ubah Status Progress</li>
-													    <li><a href="{{ route('ujiriksa.changeStatus', $fu->id) }}">Selesai</a></li>
-													    @elseif($fu->progress == 'Selesai')
-													    @if(!isset($fu->nama_pengambil))
-													    <li role="separator" class="divider"></li>
-													  	<li class="dropdown-header">Input Nama Pengambil</li>
-													    <li><a href="#" onclick="myFunction()">Diambil</a></li>
-													    <form id="form" role="form" action="{{ route('ujiriksa.storePengambil', $fu->id)}}" method="post">
-														{!! csrf_field() !!}
-													    	<input type="hidden" id="pengambil" name="nama_pengambil" />
-														</form>
-														@endif
-													    @else
-													    @endif
-													    <li role="separator" class="divider"></li>
-													    <li class="dropdown-header">Aksi Tambahan</li>
-													    <li><a href="{{ route('ujiriksa.exportPdf', $fu->id) }}" target="_blank">Export PDF</a></li>
-													    <!-- <li><a href="#">Kirim Email</a></li> -->
-													    
-													  </ul>
-													</div>
-												</div>
-								            </td>
-								        </tr>
-								    @endforeach
-								    </tbody>
-								</table>
-							</div>
-							<div class="tab-pane" role="tabpanel" id="table_service">
-								<table id="service" class="display">
-								    <thead>
-								        <tr>
-								        	<th>No Registrasi Uji</th>
-								            <th>Jenis Uji</th>
-								            <th>Progress</th>
-								            <th>Keterangan</th>
-								            <th>Tanggal Masuk</th>
-								            <th>Tanggal Selesai</th>
-								            <th>Nama Pemilik</th>
-								            <th>Aksi</th>
-								        </tr>
-								    </thead>
-								    <tbody>
-								    @foreach($service as $fu)
-								        <tr>
-								        	<td><a href="{{ route('ujiriksa.show', $fu->id) }}">{{ $fu->no_registrasi }}</a></td>
-								            <td>{{ $fu->jenis_uji }}</td>
-								        	<td>{{ $fu->progress }}</td>
-								        	<td>{{ $fu->keterangan }}</td>
-								            <td>{{ $fu->created_at->format('d-M-Y') }}</td>
-								            @if(isset($fu->done_at))
-								            <td>{{ $fu->done_at->format('d-M-Y') }}</td>
-								            @else
-								            <td>{{ "Belum Diinput" }}</td>
-								            @endif
-								            <td><a href="{{ route('customer.show', $fu->customer->id) }}">{{ $fu->customer->nama }}</a></td>
-								            <td>
-								            	<div class="btn-group dropdown" role="group" aria-label="...">
-												  <div class="btn-group navbar-right">
-													  <button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-													    Aksi <span class="caret"></span>
-													  </button>
-													  <ul class="dropdown-menu ">
-													  <li class="dropdown-header">Aksi</li>
-													  @if($fu->progress == 'Selesai')@if(!isset($fu->itemujiriksa->first()->serviceresult))
-													  	<li>
-															<a type="button" href="{{ route('service.create', $fu->id) }}">Input Hasil Service</a>
 													  	</li>
 													  @endif
 													  @endif
